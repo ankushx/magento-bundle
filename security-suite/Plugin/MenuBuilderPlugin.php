633ff08256e2ff08f0314b3bc134fe6a8ea1a4f0
ankush-tester65 <ankush.ukey@xecurify.com>
2025-11-20 13:27:16 +0530

    adding

---
+++ security-suite/Plugin/MenuBuilderPlugin.php
@@ -0,0 +1,40 @@
+<?php
+namespace MiniOrange\SecuritySuite\Plugin;
+
+class MenuBuilderPlugin
+{
+    protected $moduleManager;
+
+    public function __construct(
+        \Magento\Framework\Module\Manager $moduleManager
+    ) {
+        $this->moduleManager = $moduleManager;
+    }
+
+    public function afterGetResult(\Magento\Backend\Model\Menu\Builder $subject, $menu)
+    {
+        // Only proceed if our Security Suite is enabled
+        if (!$this->moduleManager->isEnabled('MiniOrange_SecuritySuite')) {
+            return $menu;
+        }
+
+        // Hide MiniOrange_TwoFA menu items but keep functionality
+        $menusToHide = [
+            'MiniOrange_TwoFA::TwoFA',              
+            'MiniOrange_BruteForce::BruteForce',
+            'MiniOrange_AdminActivity::AdminActivity',
+            'MiniOrange_RateLimiting::RateLimiting',
+            'MiniOrange_SSO::SSO',
+            'MiniOrange_AzureSSO::OAuth',
+            'MiniOrange_MagentoSocialLogin::SocialLogin'
+        ];
+
+        foreach ($menusToHide as $menuId) {
+            if ($menu->get($menuId)) {
+                $menu->remove($menuId);
+            }
+        }
+
+        return $menu;
+    }
+}
